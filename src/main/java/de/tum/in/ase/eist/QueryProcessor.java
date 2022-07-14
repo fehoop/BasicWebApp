012012cdaee0ca698528f199f6357b25bd1e4b49
Feng Hu <feng.hu@tum.de>
2022-07-14 13:25:45 +0200

    add codes

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,15 +6,22 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "fehoop";
+            return "fehoop";
         } else { // TODO extend the programm here
-            return "676";
+            final String[] split = query.split(",");
+            int max = Integer.MIN_VALUE;
+            if (split.length > 1) {
+                for (int i = 1; i < split.length; i++) {
+                    max = Math.max(Integer.parseInt(split[i]), max);
+                }
+            }
+            return String.valueOf(max);
         }
     }
 }
